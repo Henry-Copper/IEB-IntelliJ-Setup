15997ea6330ab669c6728485b835bcf978dd7a86
Henry-Copper <92207658+Henry-Copper@users.noreply.github.com>
2023-05-23 20:41:57 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IEB IntelliJ Setup'
+copyright = '2023, Heinrich'
+author = 'Heinrich'
 
 release = '0.1'
 version = '0.1.0'
